bd40d67c6cdd7197f3868907a0d9c3dc6ac02263
aleanon <104034507+aleanon@users.noreply.github.com>
2024-08-27 22:49:27 +0200

    Added create multiple accounts from mnemonic

--- 3_handles/src/wallet/mod.rs
+++ 3_handles/src/wallet/mod.rs
@@ -3,5 +3,6 @@ mod create_wallet;
 mod login;
 
 pub use create_account::create_account_from_mnemonic;
+pub use create_account::create_multiple_accounts_from_mnemonic;
 pub use create_wallet::create_new_wallet_with_accounts;
 pub use login::perform_login_check;
