922a69c474c85370c57808a53977029941bfcc07
aleanon <104034507+aleanon@users.noreply.github.com>
2024-09-01 22:52:16 +0200

    Added seed password to create wallet function

--- 3_handles/src/wallet/create_wallet.rs
+++ 3_handles/src/wallet/create_wallet.rs
@@ -1,29 +1,33 @@
 use bip39::Mnemonic;
-use store::Db;
+use store::AsyncDb;
 use types::{
     crypto::{EncryptedMnemonic, Password},
     Account, AppError, Network,
 };
 
-pub fn create_new_wallet_with_accounts(
+pub async fn create_new_wallet_with_accounts(
     mnemonic: &Mnemonic,
     seed_password: Option<&str>,
     app_password: &Password,
-    accounts: Vec<Account>,
+    accounts: &[Account],
     network: Network,
-    db: &mut Db,
 ) -> Result<(), AppError> {
-    let (_key, _salt) = app_password
+    let (db_key, _salt) = app_password
         .derive_new_db_encryption_key()
         .map_err(|err| AppError::Fatal(err.to_string()))?;
 
-    let encrypted_mnemonic = EncryptedMnemonic::new(mnemonic, &app_password)
+    let db = AsyncDb::new(network, db_key)
+        .await
         .map_err(|err| AppError::Fatal(err.to_string()))?;
+
+    let encrypted_mnemonic =
+        EncryptedMnemonic::new(mnemonic, seed_password.unwrap_or(""), &app_password)
+            .map_err(|err| AppError::Fatal(err.to_string()))?;
 
     crate::credentials::store_encrypted_mnemonic(&encrypted_mnemonic)
         .map_err(|err| AppError::Fatal(err.to_string()))?;
 
-    db.upsert_accounts(&accounts).ok();
+    db.upsert_accounts(accounts).await.ok();
 
     Ok(())
 }
