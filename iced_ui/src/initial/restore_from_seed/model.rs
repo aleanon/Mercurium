24ad2253278dbe41ae1b37a1f91dd1ae8a044592
aleanon <104034507+aleanon@users.noreply.github.com>
2024-10-05 23:56:49 +0200

    organized into model view update files

--- iced_ui/src/initial/restore_from_seed/model.rs
+++ iced_ui/src/initial/restore_from_seed/model.rs
@@ -46,40 +46,137 @@ impl Display for AccountSummary {
 }
 
 #[derive(Debug)]
-pub struct RestoreFromSeed {
-    pub stage: Stage,
-    pub notification: &'static str,
+pub struct Inputs {
     pub seed_phrase: SeedPhrase,
     pub seed_password: Option<Password>,
-    pub mnemonic: Option<Mnemonic>,
     pub password: Password,
     pub verify_password: Password,
+}
+
+impl Inputs {
+    pub fn new() -> Self {
+        Self {
+            seed_phrase: SeedPhrase::new(),
+            seed_password: None,
+            password: Password::new(),
+            verify_password: Password::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct KeyAndSalt {
+    pub last_task_nr: u8,
     pub db_key_salt: Option<(DataBaseKey, Salt)>,
     pub mnemonic_key_salt: Option<(Key, Salt)>,
+}
+
+impl KeyAndSalt {
+    pub fn new() -> Self {
+        Self {
+            last_task_nr: 0,
+            db_key_salt: None,
+            mnemonic_key_salt: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AccountsData {
+    pub update_account_task_nr: u8,
+    pub create_accounts_task_nr: u8,
     pub accounts: Vec<Vec<(Account, bool, AccountSummary)>>,
     pub page_index: usize,
     pub accounts_update: AccountsUpdate,
-    pub icons: HashMap<ResourceAddress, Handle>,
     pub selected_accounts: Vec<Account>,
 }
 
+impl AccountsData {
+    pub fn new(network: Network) -> Self {
+        Self {
+            update_account_task_nr: 0,
+            create_accounts_task_nr: 0,
+            accounts: Vec::new(),
+            page_index: 0,
+            accounts_update: AccountsUpdate::new(network),
+            selected_accounts: Vec::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct IconsData {
+    pub task_nr: u8,
+    pub icons: HashMap<ResourceAddress, Handle>,
+}
+
+impl IconsData {
+    pub fn new() -> Self {
+        Self {
+            task_nr: 0,
+            icons: HashMap::new(),
+        }
+    }
+}
+
+// #[derive(Debug)]
+// pub struct RestoreFromSeed {
+//     pub stage: Stage,
+//     pub notification: &'static str,
+//     pub seed_phrase: SeedPhrase,
+//     pub seed_password: Option<Password>,
+//     pub mnemonic: Option<Mnemonic>,
+//     pub password: Password,
+//     pub verify_password: Password,
+//     pub db_key_salt: Option<(DataBaseKey, Salt)>,
+//     pub mnemonic_key_salt: Option<(Key, Salt)>,
+//     pub accounts: Vec<Vec<(Account, bool, AccountSummary)>>,
+//     pub page_index: usize,
+//     pub accounts_update: AccountsUpdate,
+//     pub icons: HashMap<ResourceAddress, Handle>,
+//     pub selected_accounts: Vec<Account>,
+// }
+
+#[derive(Debug)]
+pub struct RestoreFromSeed {
+    pub stage: Stage,
+    pub notification: &'static str,
+    pub inputs: Inputs,
+    pub mnemonic: Option<Mnemonic>,
+    pub key_and_salt: KeyAndSalt,
+    pub accounts_data: AccountsData,
+    pub icons_data: IconsData,
+}
+
 impl<'a> RestoreFromSeed {
+    // pub fn new(network: Network) -> Self {
+    //     Self {
+    //         stage: Stage::EnterSeedPhrase,
+    //         notification: "",
+    //         seed_phrase: SeedPhrase::new(),
+    //         seed_password: None,
+    //         mnemonic: None,
+    //         password: Password::new(),
+    //         verify_password: Password::new(),
+    //         db_key_salt: None,
+    //         mnemonic_key_salt: None,
+    //         accounts: vec![Vec::with_capacity(20); 20],
+    //         page_index: 0,
+    //         accounts_update: AccountsUpdate::new(network),
+    //         icons: HashMap::new(),
+    //         selected_accounts: Vec::new(),
+    //     }
+    // }
+
     pub fn new(network: Network) -> Self {
         Self {
             stage: Stage::EnterSeedPhrase,
             notification: "",
-            seed_phrase: SeedPhrase::new(),
-            seed_password: None,
+            inputs: Inputs::new(),
             mnemonic: None,
-            password: Password::new(),
-            verify_password: Password::new(),
-            db_key_salt: None,
-            mnemonic_key_salt: None,
-            accounts: vec![Vec::with_capacity(20); 20],
-            page_index: 0,
-            accounts_update: AccountsUpdate::new(network),
-            icons: HashMap::new(),
-            selected_accounts: Vec::new(),
+            key_and_salt: KeyAndSalt::new(),
+            accounts_data: AccountsData::new(network),
+            icons_data: IconsData::new(),
         }
     }
 }
