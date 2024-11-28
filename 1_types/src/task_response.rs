6035f9c02154d482de890dc2089b4f7a7f644f2c
aleanon <104034507+aleanon@users.noreply.github.com>
2024-11-28 22:34:04 +0100

    Updated TaskResponse type

--- 1_types/src/task_response.rs
+++ 1_types/src/task_response.rs
@@ -5,27 +5,27 @@ pub struct TaskResponse<T> {
 }
 
 impl<T> TaskResponse<T> {
-    pub fn new(task_id: u8) -> Self {
-        Self { task_id, data: None }
-    }
-
-    pub fn new_with_data(task_id: u8, data: T) -> Self {
-        Self { task_id, data: Some(data) }
+    pub fn new(task_id: u8, data: Option<T>) -> Self {
+        Self { task_id, data, }
     }
 
     pub fn new_response(&mut self, new_response: TaskResponse<T>) {
         if self.task_id <= new_response.task_id {*self = new_response}
     }
 
-    pub fn data(&self) -> Option<&T> {
+    pub fn ref_data(&self) -> Option<&T> {
         self.data.as_ref()
+    }
+
+    pub fn ref_mut_data(&mut self) -> Option<&mut T> {
+        self.data.as_mut()
     }
 
     pub fn take_data(&mut self) -> Option<T> {
         self.data.take()
     }
 
-    pub fn discard_data(&mut self) {
+     pub fn discard_data(&mut self) {
         self.data = None;
     }
 
