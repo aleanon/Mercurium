e67c5a52ef8d31e2f70206efc3b11b220f766252
aleanon <104034507+aleanon@users.noreply.github.com>
2025-03-18 23:09:05 +0100

    Added max login attempts to app settings

--- 1_types/src/app_settings.rs
+++ 1_types/src/app_settings.rs
@@ -4,13 +4,17 @@ use crate::{theme::Theme, Network};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AppSettings {
+    pub max_login_attempts: usize,
     pub theme: Theme,
     pub network: Network,
 }
 
 impl AppSettings {
+    const MAX_LOGIN_ATTEMPTS_DEFAULT: usize = 1000;
+
     pub fn new() -> Self {
         Self {
+            max_login_attempts: Self::MAX_LOGIN_ATTEMPTS_DEFAULT,
             theme: Theme::Dark,
             network: Network::default(),
         }
