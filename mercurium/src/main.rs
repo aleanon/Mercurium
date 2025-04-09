fd5ec6fdf0fbc517260188707d8b1e17c23b1929
aleanon <104034507+aleanon@users.noreply.github.com>
2025-04-09 22:16:13 +0200

    Removed the hot reload impl from main

--- mercurium/src/main.rs
+++ mercurium/src/main.rs
@@ -1,19 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 
-#[cfg(feature = "reload")]
-#[hot_lib_reloader::hot_module(dylib = "styles")]
-mod hot_lib {
-    use styles::*;
-    // println!("hot reloading");
-    hot_functions_from_file!("iced_ui/styles/src/lib.rs", ignore_no_mangle = true);
-    
-    // hot_functions_from_file!("iced_ui/theme/src/styles/container.rs", ignore_no_mangle = true);
-}
-
-// #[cfg(feature="reload")]
-// use hot_lib::*;
-
 #[tokio::main]
 async fn main() -> Result<(), iced_ui::Error> {
     #[cfg(all(debug_assertions, target_os = "windows"))]
@@ -23,12 +10,3 @@ async fn main() -> Result<(), iced_ui::Error> {
     #[cfg(any(target_os = "windows", target_os = "linux", target_os = "macos"))]
     iced_ui::App::run()
 }
-
-// fn main() {
-//     loop {
-//         println!("1");
-
-//         thread::sleep(Duration::from_millis(10000));
-//     }
-// }
-
