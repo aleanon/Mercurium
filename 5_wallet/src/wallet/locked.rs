384820d097d523d127eefec9398a49c40bd689fb
aleanon <forsvantn@protonmail.com>
2025-05-25 23:38:48 +0200

    removed unused dependencies

--- 5_wallet/src/wallet/locked.rs
+++ 5_wallet/src/wallet/locked.rs
@@ -1,14 +1,12 @@
 use std::sync::Arc;
 
-use deps::*;
-
 use store::{AppDataDb, DataBase, IconsDb};
 use thiserror::Error;
-use types::{crypto::{Password, Key}, AppSettings};
+use types::crypto::{Key, Password};
 
-use crate::{settings::Settings, wallet::WalletState, WalletData};
+use crate::{WalletData, settings::Settings, wallet::WalletState};
 
-use super::{unlocked::Unlocked, Wallet};
+use super::{Wallet, unlocked::Unlocked};
 
 #[derive(Debug, Error)]
 pub enum LoginError {
@@ -17,12 +15,12 @@ pub enum LoginError {
     #[error("Max login attempts reached")]
     MaxAttemptsReached,
     #[error("Unrecoverable error")]
-    Unrecoverable
+    Unrecoverable,
 }
 
 pub enum LoginResponse {
     Success(Wallet<Unlocked>, bool),
-    Failed(Wallet<Locked>, LoginError)
+    Failed(Wallet<Locked>, LoginError),
 }
 
 #[derive(Debug, Clone)]
@@ -33,43 +31,76 @@ pub struct Locked {
 
 impl Locked {
     pub fn new(is_initial_login: bool) -> Self {
-        Self { attempts: 0, is_initial_login }
+        Self {
+            attempts: 0,
+            is_initial_login,
+        }
     }
 }
 
-impl WalletState for Locked{}
+impl WalletState for Locked {}
 
 impl Wallet<Locked> {
     pub async fn login_with_password(self, password: Password) -> LoginResponse {
         if self.state.attempts >= self.max_login_attempts() {
-            return LoginResponse::Failed(self, LoginError::MaxAttemptsReached)
+            return LoginResponse::Failed(self, LoginError::MaxAttemptsReached);
         }
-        
+
         let Ok(salt) = handles::credentials::get_db_encryption_salt() else {
-            return LoginResponse::Failed(self, LoginError::Unrecoverable)
+            return LoginResponse::Failed(self, LoginError::Unrecoverable);
         };
 
         let key = Key::<DataBase>::new(password.as_str(), &salt);
 
-        let mut wallet = match handles::wallet::perform_login_check(self.wallet_data.settings.network, &password).await {
-            Ok(_) => Wallet { state: Unlocked::new(key), wallet_data: self.wallet_data},
-            Err(_) => {
-                return LoginResponse::Failed(self, LoginError::IncorrectPassword)
-            }
+        let mut wallet = match handles::wallet::perform_login_check(
+            self.wallet_data.settings.network,
+            &password,
+        )
+        .await
+        {
+            Ok(_) => Wallet {
+                state: Unlocked::new(key),
+                wallet_data: self.wallet_data,
+            },
+            Err(_) => return LoginResponse::Failed(self, LoginError::IncorrectPassword),
         };
-        
+
         if self.state.is_initial_login {
-            let Ok(app_data_db) = AppDataDb::get_or_init(wallet.wallet_data.settings.network, wallet.state.key.clone()).await else {
-                return LoginResponse::Failed(Wallet {state: Locked::new(true), wallet_data: wallet.wallet_data}, LoginError::Unrecoverable)
+            let Ok(app_data_db) = AppDataDb::get_or_init(
+                wallet.wallet_data.settings.network,
+                wallet.state.key.clone(),
+            )
+            .await
+            else {
+                return LoginResponse::Failed(
+                    Wallet {
+                        state: Locked::new(true),
+                        wallet_data: wallet.wallet_data,
+                    },
+                    LoginError::Unrecoverable,
+                );
             };
 
-            let Ok(icons_db) = IconsDb::get_or_init(wallet.wallet_data.settings.network, wallet.state.key.clone()).await else {
-                return LoginResponse::Failed(Wallet {state: Locked::new(true), wallet_data: wallet.wallet_data}, LoginError::Unrecoverable)
+            let Ok(icons_db) = IconsDb::get_or_init(
+                wallet.wallet_data.settings.network,
+                wallet.state.key.clone(),
+            )
+            .await
+            else {
+                return LoginResponse::Failed(
+                    Wallet {
+                        state: Locked::new(true),
+                        wallet_data: wallet.wallet_data,
+                    },
+                    LoginError::Unrecoverable,
+                );
             };
 
             let resources = Arc::make_mut(&mut wallet.wallet_data.resource_data);
 
-            resources.load_resource_data_from_disk(app_data_db, icons_db).await
+            resources
+                .load_resource_data_from_disk(app_data_db, icons_db)
+                .await
                 .inspect_err(|err| eprintln!("Failed to load resource data: {err}"))
                 .ok();
         }
@@ -92,6 +123,9 @@ impl Wallet<Locked> {
 
 impl Default for Wallet<Locked> {
     fn default() -> Self {
-        Self::new(Locked::new(true), WalletData::new(Settings::load_from_disk_or_default()))
+        Self::new(
+            Locked::new(true),
+            WalletData::new(Settings::load_from_disk_or_default()),
+        )
     }
 }
--- 5_wallet/src/wallet/wallet_data.rs
+++ 5_wallet/src/wallet/wallet_data.rs
@@ -1,14 +1,16 @@
 use std::{collections::HashMap, sync::Arc};
 
-use deps::{futures::TryFutureExt, tokio::{self, task::JoinHandle}};
+use deps::tokio::{self, task::JoinHandle};
 use store::{AppDataDb, DataBase, DbError, IconsDb};
-use types::{Account, address::ResourceAddress, crypto::{EncryptedMnemonicError, Key, Password}, AppError, Network};
-
+use types::{
+    Account, AppError, Network,
+    address::ResourceAddress,
+    crypto::{EncryptedMnemonicError, Key, Password},
+};
 
 use crate::settings::Settings;
 
 use super::{create_account_from_mnemonic, resource_data::ResourceData};
-
 
 #[derive(Debug, Clone)]
 pub struct WalletData {
@@ -16,17 +18,23 @@ pub struct WalletData {
     pub settings: Settings,
 }
 
-
 impl WalletData {
     pub fn new(settings: Settings) -> Self {
-        Self { resource_data: Arc::new(ResourceData::new()), settings }
+        Self {
+            resource_data: Arc::new(ResourceData::new()),
+            settings,
+        }
     }
 
     pub fn set_network(&mut self, network: Network) {
         self.settings.network = network;
     }
 
-    pub async fn save_resource_icons_to_disk(&self, icons: HashMap<ResourceAddress, Vec<u8>> , db_key: Key<DataBase>) -> Result<(), DbError> {
+    pub async fn save_resource_icons_to_disk(
+        &self,
+        icons: HashMap<ResourceAddress, Vec<u8>>,
+        db_key: Key<DataBase>,
+    ) -> Result<(), DbError> {
         let db = IconsDb::get_or_init(self.settings.network, db_key).await?;
         db.upsert_resource_icons(icons).await?;
         Ok(())
@@ -38,24 +46,39 @@ impl WalletData {
         self.resource_data.save_resource_data_to_disk(db).await
     }
 
-    pub fn create_new_account(&mut self, account_name: String, password: Password, key: Key<DataBase>) -> JoinHandle<Result<Account, AppError>> {        
-        let (id, derivation_index) = self.resource_data.accounts.values()
-        .fold((0, 0), |(mut id, mut index), account| {
-            if account.id >= id {id = account.id + 1}
-            let der_index = account.derivation_index();
-            if der_index >= index {index = der_index + 1};
-            (id, index)
-        });
+    pub fn create_new_account(
+        &mut self,
+        account_name: String,
+        password: Password,
+        key: Key<DataBase>,
+    ) -> JoinHandle<Result<Account, AppError>> {
+        let (id, derivation_index) =
+            self.resource_data
+                .accounts
+                .values()
+                .fold((0, 0), |(mut id, mut index), account| {
+                    if account.id >= id {
+                        id = account.id + 1
+                    }
+                    let der_index = account.derivation_index();
+                    if der_index >= index {
+                        index = der_index + 1
+                    };
+                    (id, index)
+                });
         let network = self.settings.network;
-        
+
         tokio::spawn(async move {
             let encrypted_mnemonic = handles::credentials::get_encrypted_mnemonic()?;
-            let (mnemonic, seed_password) = encrypted_mnemonic.decrypt_mnemonic(&password)
+            let (mnemonic, seed_password) = encrypted_mnemonic
+                .decrypt_mnemonic(&password)
                 .map_err(|err| match err {
-                    EncryptedMnemonicError::FailedToDecryptData => AppError::NonFatal(types::Notification::Info("Wrong password".to_string())),
-                    _ => AppError::Fatal(err.to_string())
+                    EncryptedMnemonicError::FailedToDecryptData => {
+                        AppError::NonFatal(types::Notification::Info("Wrong password".to_string()))
+                    }
+                    _ => AppError::Fatal(err.to_string()),
                 })?;
-    
+
             let account = create_account_from_mnemonic(
                 &mnemonic,
                 Some(seed_password.as_str()),
@@ -64,13 +87,13 @@ impl WalletData {
                 account_name,
                 network,
             );
-    
-            let db = AppDataDb::get(network).ok_or(AppError::Fatal("Database not found".to_string()))?;
-            db.upsert_account(account.clone()).await
-                .map_err(|err|AppError::NonFatal(types::Notification::Info(err.to_string())))?;
+
+            let db =
+                AppDataDb::get(network).ok_or(AppError::Fatal("Database not found".to_string()))?;
+            db.upsert_account(account.clone())
+                .await
+                .map_err(|err| AppError::NonFatal(types::Notification::Info(err.to_string())))?;
             Ok(account)
         })
     }
 }
-
-
